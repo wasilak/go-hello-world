2361986d6129e16275070597f8cd4c351ae351e3
Piotr Boruc <piotr.m.boruc@gmail.com>
2021-04-15 11:14:05 +0200

    code cleanup

--- main.go
+++ main.go
@@ -25,14 +25,14 @@ type HealthResponse struct {
 
 // APIResponseRequest type
 type APIResponseRequest struct {
-	Host       string   `json:"host"`
-	RemoteAddr string   `json:"remote_addr"`
-	RequestURI string   `json:"request_uri"`
-	Method     string   `json:"method"`
-	Proto      string   `json:"proto"`
-	UserAgent  string   `json:"user_agent"`
-	URL        *url.URL `json:"url"`
-	Headers http.Header `json:"headers"`
+	Host       string      `json:"host"`
+	RemoteAddr string      `json:"remote_addr"`
+	RequestURI string      `json:"request_uri"`
+	Method     string      `json:"method"`
+	Proto      string      `json:"proto"`
+	UserAgent  string      `json:"user_agent"`
+	URL        *url.URL    `json:"url"`
+	Headers    http.Header `json:"headers"`
 }
 
 // APIResponse type
@@ -89,7 +89,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 		Method:     r.Method,
 		Proto:      r.Proto,
 		UserAgent:  r.UserAgent(),
-		Headers: r.Header,
+		Headers:    r.Header,
 	}
 
 	session.Values["apiResponse"] = response
